1b4536762af1328ae93ec1ca9141f3c3fd5fe4d1
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-01-03 22:57:52 -0600

    first commit and changed gitignore

---
+++ src/main/java/com/koko/springboot/TodoJavaApp/TodoJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.koko.springboot.TodoJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koko/springboot/TodoJavaApp/TodoJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koko.springboot.TodoJavaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
